15672c14f2844c7751bab4d6caf4c99f71d88ee8
j4ndrw <murjan_andreialin@yahoo.com>
2023-12-02 13:55:10 +0200

    1: Day one finished!!!

--- one/src/main.rs
+++ one/src/main.rs
@@ -1,37 +1,55 @@
 use std::error::Error;
 
 trait Calibratable {
-    fn word_to_int(&self) -> String;
+    fn convert_words_to_digits(&self) -> String;
+    fn get_last_digit_from_worded_number(&self) -> Option<char>;
     fn get_calibration_value(&self) -> i32;
 }
 
 impl Calibratable for str {
-    fn word_to_int(&self) -> String {
+    fn get_last_digit_from_worded_number(&self) -> Option<char> {
+        if self.len() < 3 {
+            return None;
+        }
+        return match self {
+            "one" => Some('1'),
+            "two" => Some('2'),
+            "three" => Some('3'),
+            "four" => Some('4'),
+            "five" => Some('5'),
+            "six" => Some('6'),
+            "seven" => Some('7'),
+            "eight" => Some('8'),
+            "nine" => Some('9'),
+            s => s
+                .chars()
+                .skip(1)
+                .collect::<String>()
+                .get_last_digit_from_worded_number(),
+        };
+    }
+    fn convert_words_to_digits(&self) -> String {
         let mut buf = String::new();
         let mut new_str = String::new();
 
         for c in self.chars() {
             buf.push(c);
-            let matched_str = match buf.as_str() {
-                "zero" | "twenty" | "thirty" | "forty" | "fifty" | "sixty" | "seventy"
-                | "eighty" | "ninety" => Some("0"),
-                "one" => Some("1"),
-                "two" => Some("2"),
-                "three" => Some("3"),
-                "four" => Some("4"),
-                "five" => Some("5"),
-                "six" => Some("6"),
-                "seven" => Some("7"),
-                "eight" => Some("8"),
-                "nine" => Some("9"),
-                _ => None,
-            };
-            if let Some(digit) = matched_str {
-                buf.clear();
-                new_str.push_str(digit);
+
+            let buf_snapshot = buf.clone();
+            let digit = buf_snapshot.get_last_digit_from_worded_number();
+
+            let aggregate = if let Some(digit) = digit {
+                Some(digit)
             } else if c.is_digit(10) {
+                Some(c)
+            } else {
+                None
+            };
+
+            if let Some(aggregate) = aggregate {
                 buf.clear();
-                new_str.push(c);
+                buf.push(buf_snapshot.chars().last().unwrap());
+                new_str.push(aggregate);
             }
         }
         return new_str;
@@ -39,8 +57,10 @@ impl Calibratable for str {
 
     fn get_calibration_value(&self) -> i32 {
         self.chars()
-            .filter(|c| c.is_digit(10))
             .fold(String::new(), |mut acc, c| {
+                if !c.is_digit(10) {
+                    return acc;
+                }
                 if acc.len() <= 1 {
                     acc.push(c);
                     return acc;
@@ -69,25 +89,25 @@ fn first_solution(input: &str) -> i32 {
 }
 
 fn second_solution(input: &str) -> i32 {
-    let x = input
+    return input
         .split('\n')
         .filter(|line| !line.is_empty())
-        .map(|line| line.word_to_int());
-    println!("x: {:#?}", &x.clone().collect::<String>());
-    return x.fold(0, |sum, line| sum + line.get_calibration_value());
+        .map(|line| line.convert_words_to_digits())
+        .fold(0, |sum, line| sum + line.get_calibration_value());
 }
 
 fn main() -> Result<(), Box<dyn Error>> {
-    // let part1_sample_output = first_solution(include_str!("../part1-sample.txt"));
-    // let part1_real_output = first_solution(include_str!("../part1-real.txt"));
+    let part1_sample_output = first_solution(include_str!("../part1-sample.txt"));
+    let part1_real_output = first_solution(include_str!("../part1-real.txt"));
 
     let part2_sample_output = second_solution(include_str!("../part2-sample.txt"));
-    // let part2_real_output = second_solution(include_str!("../part2-real.txt"));
+    let part2_real_output = second_solution(include_str!("../part2-real.txt"));
 
-    // println!("part1-sample: {:#?}", part1_sample_output);
-    // println!("part1-real: {:#?}", part1_real_output);
+    println!("part1-sample: {:#?}", part1_sample_output);
+    println!("part1-real: {:#?}", part1_real_output);
 
     println!("part2-sample: {:#?}", part2_sample_output);
-    // println!("part2-real: {:#?}", part2_real_output);
+    println!("part2-real: {:#?}", part2_real_output);
+
     return Ok(());
 }
